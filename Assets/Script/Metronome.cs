cee50c08e5ed29f89c7c3d3d91d4432a50e9a7ec
Cleanhea <caduga1055@gmail.com>
2025-03-25 18:12:02 +0900

    초기파일 업로드
    ㅁㄴㅇㄹ

---
+++ Assets/Script/Metronome.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using FMOD.Studio;
+using FMODUnity;
+using UnityEngine;
+
+public class Metronome : MonoBehaviour
+{
+    public float bpm = 120f;
+
+    EventInstance metronomeInstance;
+
+    void Start()
+    {
+    }
+
+}
