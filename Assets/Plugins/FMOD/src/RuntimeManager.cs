ab4919648a2c2e5baf3e70ba71453e021611bfc5
Cleanhea <caduga1055@gmail.com>
2025-03-25 19:31:06 +0900

    Revert "초기파일 업로드2"
    This reverts commit da7d6afa63a860cb209c97dd07d1cb6d64575ab7.

--- Assets/Plugins/FMOD/src/RuntimeManager.cs
+++ Assets/Plugins/FMOD/src/RuntimeManager.cs
@@ -336,10 +336,14 @@ retry:
             result = coreSystem.setSoftwareFormat(sampleRate, speakerMode, 0);
             CheckInitResult(result, "FMOD.System.setSoftwareFormat");
 
-            if (dspBufferLength > 0 && dspBufferCount > 0)
-            {
-                result = coreSystem.setDSPBufferSize(dspBufferLength, dspBufferCount);
-                CheckInitResult(result, "FMOD.System.setDSPBufferSize");
+            // if (dspBufferLength > 0 && dspBufferCount > 0)
+            // {
+            //     result = coreSystem.setDSPBufferSize(dspBufferLength, dspBufferCount);
+            //     CheckInitResult(result, "FMOD.System.setDSPBufferSize");
+            // }
+            if(true){
+                result = coreSystem.setDSPBufferSize(64,2);
+                CheckInitResult(result,"FMOD.System.setDSPBufferSize");
             }
 
             result = coreSystem.setAdvancedSettings(ref advancedSettings);
